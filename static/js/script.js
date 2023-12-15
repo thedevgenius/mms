ba06c464bf54300d294b0d4d0fb4824f32bb77da
rupam office <thedevgenius83@gmail.com>
2023-12-15 17:08:54 +0530

    Update

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,12 @@ $( document ).ready(function() {
         e.preventDefault();
         $(this).next().slideToggle();
     });
+
+
+    var swiper = new Swiper(".team-slide", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
 });
